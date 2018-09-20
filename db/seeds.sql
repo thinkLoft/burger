ef1a23e6922e517e69a3aeb1e8469363715dfe17
Kareem Lofters <kareem.lofters@gmail.com>
2018-09-20 01:06:30 -0400

    completed server boilerplate, updated database w/ seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sushi Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ghost Burger', true);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var PORT = process.env.PORT || 8080;
 
@@ -14,16 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require('express-handlebars');
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Import routes and give the server access to them.
-var routes = require('./controllers/burgers_controller.js');
+// // Import routes and give the server access to them.
+// var routes = require('./controllers/burgers_controller.js');
 
-app.use(routes);
+// app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
