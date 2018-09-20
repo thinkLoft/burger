d8243a2ecd8182454fc44828027d10a8edfc20d1
Kareem Lofters <kareem.lofters@gmail.com>
2018-09-20 02:32:58 -0400

    working application, version 0.1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 
 var app = express();
 
@@ -18,13 +18,16 @@ app.use(bodyParser.json());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// // Import routes and give the server access to them.
-// var routes = require('./controllers/burgers_controller.js');
+// Import routes and give the server access to them.
+var routes = require('./controllers/burgers_controller.js');
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log('Server listening on: http://localhost:' + PORT);
 });
+
+// Import MySQL connection.
+var connection = require("../burger/config/connection.js");
